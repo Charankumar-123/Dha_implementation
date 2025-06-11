da27ae6b4301764921a00e6f68ed2633fd029aff
Charankumar-123 <chelikamcharankumar@gmail.com>
2025-06-11 18:43:12 +0530

    Update UploadERxRequestRepository.java

--- DHA/src/main/java/com/ac/dha/repository/UploadERxRequestRepository.java
+++ DHA/src/main/java/com/ac/dha/repository/UploadERxRequestRepository.java
@@ -1,10 +1,10 @@
 package com.ac.dha.repository;
 
+
+import com.ac.dha.entities.UploadErxRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ac.dha.entties.UploadERxRequest;
-
 @Repository
-public interface UploadERxRequestRepository extends JpaRepository<UploadERxRequest, Long> {
+public interface UploadErxRequestRepository extends JpaRepository<UploadErxRequest, Long> {
 }
